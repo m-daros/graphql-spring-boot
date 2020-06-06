95e6b5bf4f5ce4d7c0e11da6f6b93df132397f66
Massimo Da Ros <massimo.daros@bitrock.it>
2020-06-06 23:18:15 +0200

    Removed unused Websocket handler

--- src/main/java/mdaros/training/graphql/spring/boot/configuration/SpringConfig.java
+++ src/main/java/mdaros/training/graphql/spring/boot/configuration/SpringConfig.java
@@ -1,38 +1,8 @@
 package mdaros.training.graphql.spring.boot.configuration;
 
-import mdaros.training.graphql.spring.boot.websocket.SubscriptionWebsocketHandler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
-public class SpringConfig implements WebSocketConfigurer  {
+public class SpringConfig {
 
-	@Override
-	public void registerWebSocketHandlers ( WebSocketHandlerRegistry webSocketHandlerRegistry ) {
-
-		webSocketHandlerRegistry.addHandler ( subscriptionsWebSocketHandler (), "/graphql" )
-				.setAllowedOrigins( "*" )
-				.withSockJS ();
-	}
-
-	@Bean
-	public ServletServerContainerFactoryBean createWebSocketContainer () {
-
-		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean ();
-		container.setMaxTextMessageBufferSize ( 1024 * 1024 );
-		container.setMaxSessionIdleTimeout ( 30L * 1000 );
-
-		return container;
-	}
-
-	@Bean
-	public WebSocketHandler subscriptionsWebSocketHandler () {
-
-		return new PerConnectionWebSocketHandler ( SubscriptionWebsocketHandler.class );
-	}
 }
